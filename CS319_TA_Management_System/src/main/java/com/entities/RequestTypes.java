21794f81aa93ccf6d0d465ec023e36be46cb3602
1enesozdemir1 <91872892+1enesozdemir1@users.noreply.github.com>
2025-05-07 19:02:20 +0300

    Create RequestTypes.java

---
+++ CS319_TA_Management_System/src/main/java/com/entities/RequestTypes.java
@@ -0,0 +1,9 @@
+package com.entities;
+
+public enum RequestTypes {
+    AUTOMATIC_SWAP_REQUEST,
+    LEAVE_OF_ABSENCE_REQUEST,
+    MANUEL_SWAP_REQUEST,
+    TASK_SUBMISSION_REQUEST,
+    UNKNOWN
+}
