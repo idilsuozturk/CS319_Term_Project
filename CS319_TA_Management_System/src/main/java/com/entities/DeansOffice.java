fa542e52132cab877e1d8201c018a148435b7e10
1enesozdemir1 <91872892+1enesozdemir1@users.noreply.github.com>
2025-05-07 18:54:48 +0300

    Update DeansOffice.java

--- CS319_TA_Management_System/src/main/java/com/entities/DeansOffice.java
+++ CS319_TA_Management_System/src/main/java/com/entities/DeansOffice.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "deansoffice")
+@Table(name = "deans_office")
 public class DeansOffice extends Staff{
     public DeansOffice() {
         super();
