f5986c9051eee1f8331e6bb9bd999b7fad9e4d0c
1enesozdemir1 <91872892+1enesozdemir1@users.noreply.github.com>
2025-05-07 18:59:33 +0300

    Update Notification.java

--- CS319_TA_Management_System/src/main/java/com/entities/Notification.java
+++ CS319_TA_Management_System/src/main/java/com/entities/Notification.java
@@ -12,62 +12,28 @@ public class Notification {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer notificationId;
+    private Integer notificationID;
+    private Integer requestID;
 
-    private Boolean isRead;
-    private Integer userId;
-    private String description;
-
-    // same thing
     public Notification() {
-        this.notificationId = null;
-        this.isRead = false;;
-        this.userId = null;
-
-
-        this.description = null;
+        requestID = -1;
     }
 
 
-    public Notification(Integer notificationId, Boolean isRead, Integer userId, String description) {
-        this.notificationId = notificationId;
-        this.isRead = isRead;
-        this.userId = userId;
-        this.description = description;
+    public Notification(Integer requestID) {
+        this.requestID = requestID;
     }
 
     // getters and 
-    public Integer getNotificationId() {
-        return notificationId;
+    public Integer getID() {
+        return notificationID;
     }
 
-    public void setNotificationId(Integer notificationId) {
-        this.notificationId = notificationId;
+    public Integer getRequestId() {
+        return requestID;
     }
 
-    public Boolean getIsRead() {
-        return isRead;
-    }
-
-    public void setIsRead(Boolean isRead) {
-
-        // questinable to implement
-        this.isRead = isRead;
-    }
-
-    public Integer getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Integer userId) {
-        this.userId = userId;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
+    public void setRequestId(Integer requestID) {
+        this.requestID = requestID;
     }
 }
