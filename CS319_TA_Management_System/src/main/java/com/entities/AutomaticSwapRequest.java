a031c2bd362fb369c09a857b1b2c29b28abe6077
1enesozdemir1 <91872892+1enesozdemir1@users.noreply.github.com>
2025-05-07 18:46:49 +0300

    Update AutomaticSwapRequest.java

--- CS319_TA_Management_System/src/main/java/com/entities/AutomaticSwapRequest.java
+++ CS319_TA_Management_System/src/main/java/com/entities/AutomaticSwapRequest.java
@@ -6,17 +6,24 @@ import jakarta.persistence.*;
 public class AutomaticSwapRequest extends Request {
     private int firstTAID;
     private int secondTAID;
+    private int firstTAsProctoringAssignmentID;
+    private int secondTAsProctoringAssignmentID;
 
     public AutomaticSwapRequest(){
         super();
+        setRequestType(RequestTypes.AUTOMATIC_SWAP_REQUEST);
         this.firstTAID = -1;
         this.secondTAID = -1;
+        this.firstTAsProctoringAssignmentID = -1;
+        this.secondTAsProctoringAssignmentID = -1;
     }
 
-    public AutomaticSwapRequest(String requestDate, int ownerID, int firstTAID, int secondTAID){
-        super(requestDate, "Automatic Swap", ownerID);
+    public AutomaticSwapRequest(String requestDate, int ownerID, String message, int firstTAID, int secondTAID, int firstTAsProctoringAssignmentID, int secondTAsProctoringAssignmentID){
+        super(requestDate, RequestTypes.AUTOMATIC_SWAP_REQUEST, ownerID, message);
         this.firstTAID = firstTAID;
         this.secondTAID = secondTAID;
+        this.firstTAsProctoringAssignmentID = firstTAsProctoringAssignmentID;
+        this.secondTAsProctoringAssignmentID = secondTAsProctoringAssignmentID;
     }
 
     public int getFirstTAID(){
@@ -33,5 +40,21 @@ public class AutomaticSwapRequest extends Request {
 
     public void setSecondTAID(int secondTAID){
         this.secondTAID = secondTAID;
+    }
+
+    public int getFirstTAsProctoringAssignmentID(){
+        return this.firstTAsProctoringAssignmentID;
+    }
+
+    public void setFirstTAsProctoringAssignmentID(int firstTAsProctoringAssignmentID){
+        this.firstTAsProctoringAssignmentID = firstTAsProctoringAssignmentID;
+    }
+
+    public int getSecondTAsProctoringAssignmentID(){
+        return this.secondTAsProctoringAssignmentID;
+    }
+
+    public void setSecondTAsProctoringAssignmentID(int secondTAsProctoringAssignmentID){
+        this.secondTAsProctoringAssignmentID = secondTAsProctoringAssignmentID;
     }
 }
