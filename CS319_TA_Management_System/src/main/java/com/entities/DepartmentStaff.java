a9f4ce3ed2c98c279eb31af507369fa3f331942f
1enesozdemir1 <91872892+1enesozdemir1@users.noreply.github.com>
2025-05-07 18:55:13 +0300

    Update DepartmentStaff.java

--- CS319_TA_Management_System/src/main/java/com/entities/DepartmentStaff.java
+++ CS319_TA_Management_System/src/main/java/com/entities/DepartmentStaff.java
@@ -5,16 +5,12 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "department_staff")
 public class DepartmentStaff extends Staff {
-  
 
     public DepartmentStaff() {
         super();
-   
+        setRole(Roles.DEPARTMENT_STAFF);
     }
-    public DepartmentStaff(String name, String email, String userName, String password, String departmentCode, String title) {
-        super(name, email, userName, password, departmentCode, title );
-        super.setRole(Roles.DEPARTMENT_STAFF);
-      
+    public DepartmentStaff(String name, String email, String username, String password, String departmentCode) {
+        super(name, email, username, password, departmentCode, Roles.DEPARTMENT_STAFF);
     }
-
 }
