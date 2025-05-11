4f216a15269ef3a115ee7c26aa2b608ce17fe00f
idilsuozturk <suidil1234@gmail.com>
2025-05-11 19:25:11 +0300

    updates in swap proctor buttons

--- CS319_TA_Management_System/src/main/java/com/controllers/TAController.java
+++ CS319_TA_Management_System/src/main/java/com/controllers/TAController.java
@@ -1,5 +1,17 @@
 package com.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -15,19 +27,6 @@ import com.services.NotificationService;
 import com.services.ProctoringAssignmentService;
 import com.services.TAService;
 import com.services.TaskSubmissionRequestService;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/tas")
@@ -105,7 +104,7 @@ public class TAController {
         return taService.showTAs(id);
     }
 
-    @GetMapping("/{id}/showpossibleprocswapreq")
+    @GetMapping("/showpossibleprocswapreq")
     public List<ProctoringAssignment> showPossibleProctoringAssignments(@RequestParam int receiverID, @RequestParam int proctoringAssignmentID){
         return proctoringAssignmentService.showPossibleProctoringAssignments(receiverID, proctoringAssignmentID);
     }
